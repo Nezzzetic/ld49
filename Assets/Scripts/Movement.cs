faa7cc0a81dfe2ab64880a2234906aa4f30891f5
Nezzzetic <nezzzetic@gmail.com>
2021-10-01 21:57:38 +0300

    warmup

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class Movement : MonoBehaviour
     float time=5;
     float speed=1;
     public float timeMax=5;
+    public float distMax=200;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,7 @@ public class Movement : MonoBehaviour
     void Update()
     {
         Move();
+        CheckLose();
         time-=Time.deltaTime;
         if (time<0) {
             Spin();
@@ -36,6 +39,10 @@ public class Movement : MonoBehaviour
     void OnMouseDown() {
         speed++;
         Spin();
+    }
+
+    void CheckLose() {
+        if (Vector3.Distance(transform.position,new Vector3(0,0,0))>distMax) SceneManager.LoadScene(0);
     }
     
 

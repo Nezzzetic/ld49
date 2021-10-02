26a2e59307e9ad0dd2e1217e0e02d145ca1a7c54
Nezzzetic <nezzzetic@gmail.com>
2021-10-02 16:36:59 +0300

    prototype3

--- Assets/Scripts/ControlWithLatency.cs
+++ Assets/Scripts/ControlWithLatency.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 
 public class ControlWithLatency : MonoBehaviour
 {
-    
+    public Transform SignalObj;
+    public float[] SignalDistances;
+    public int[] LatencyValues;
+    public int SignalLevel;
     public SimpleMovement RCMovement;
     public int LatencyValue;
+    public int LatencyMax;
+    
     private List<int> moveCommands;
     private List<int> rotateCommands;
+    private int lastMoveCommand;
+    private int lastRotCommand;
 
     private int movePressed;
     private int rotPressed;
@@ -18,10 +25,11 @@ public class ControlWithLatency : MonoBehaviour
     {
         moveCommands= new List<int>();
         rotateCommands= new List<int>();
-        for (var i=0;i<LatencyValue;i++) {
-            moveCommands.Add(0);
-            rotateCommands.Add(0);
+        for (var i=0;i<LatencyMax;i++) {
+            moveCommands.Add(10);
+            rotateCommands.Add(10);
         }
+        changeLatency();
     }
 
     // Update is called once per frame
@@ -58,23 +66,112 @@ public class ControlWithLatency : MonoBehaviour
         if (rotateCommands.Count > 0)
             removeRotActionInQ();
     }
+    
+    void ProcessSignal()
+    {
+        if (Vector3.Distance(SignalObj.position, transform.position)<SignalDistances[0])
+        {
+            if (SignalLevel!=1) {
+                SignalLevel = 1;
+                changeLatency();
+            }
+            return;
+        }
+        if (Vector3.Distance(SignalObj.position, transform.position)<SignalDistances[1])
+        {
+            if (SignalLevel!=2) {
+                SignalLevel = 2;
+                changeLatency();
+            }
+            return;
+        }
+        if (Vector3.Distance(SignalObj.position, transform.position)<SignalDistances[2])
+        {
+            if (SignalLevel!=3) {
+                SignalLevel = 3;
+                changeLatency();
+            }
+            return;
+        }
+        if (Vector3.Distance(SignalObj.position, transform.position)<SignalDistances[3])
+        {
+            if (SignalLevel!=4) {
+                SignalLevel = 4;
+                changeLatency();
+            }
+            return;
+        }
+        if (Vector3.Distance(SignalObj.position, transform.position)<SignalDistances[4])
+        {
+            if (SignalLevel!=5) {
+                SignalLevel = 5;
+                changeLatency();
+            }
+            return;
+        }
+        
+    }
+    
+    void changeLatency()
+    {
+        switch (SignalLevel)
+        {
+            case 1:
+                LatencyValue = LatencyValues[0];
+                break;
+            case 2:
+                LatencyValue = LatencyValues[1];
+                break;
+            case 3:
+                LatencyValue = LatencyValues[2];
+                break;
+            case 4:
+                LatencyValue = LatencyValues[3];
+                break;
+            case 5:
+                LatencyValue = LatencyValues[4];
+                break;
+        }
+        Debug.Log("changeLatency");
+        for (int i = LatencyValue+1; i < LatencyMax; i++)
+        {
+            moveCommands[i] = 10;
+            rotateCommands[i] = 10;
+        }
+    }
 
     private void FixedUpdate()
     {
         ProcessInput();
         ProcessQ();
+        ProcessSignal();
     }
 
     void addMoveActionInQ(int flag)
     {
-        moveCommands.Add(flag);
+        
+        moveCommands[LatencyValue] = flag;
     }
+    
+    
     
     //TODO поменять на подходящий для такой очереди "лист"
     void removeMoveActionInQ()
     {
         var moveAction = moveCommands[0];
         moveCommands.RemoveAt(0);
+        moveCommands.Add(10);
+
+        if (moveAction == 10)
+        {
+            if (lastMoveCommand == 1)
+            {
+                RCMovement.MoveForwardAction(); 
+                return;
+            }
+        }
+        else
+            lastMoveCommand = moveAction;
         if (moveAction == 1)
         {
             RCMovement.MoveForwardAction();
@@ -83,15 +180,28 @@ public class ControlWithLatency : MonoBehaviour
     
     void addRotActionInQ(int direction)
     {
-        rotateCommands.Add(direction);
+        rotateCommands[LatencyValue] = direction;
     }
     
     //TODO поменять на подходящий для такой очереди "лист"
     void removeRotActionInQ()
     {
+        Debug.Log(rotateCommands[0]+" "+rotateCommands[1]+" "+rotateCommands[2]+" "+rotateCommands[3]+" "+rotateCommands[4]);
         var rotAction = rotateCommands[0];
         rotateCommands.RemoveAt(0);
-        if (rotAction != 0)
+        rotateCommands.Add(10);
+        
+        if (rotAction == 10)
+        {
+            if (lastRotCommand == 1 || lastRotCommand==-1)
+            {
+                RCMovement.RotateAction(lastRotCommand);
+                return;
+            }
+        }
+        else
+            lastRotCommand = rotAction;
+        if (rotAction == 1 || rotAction==-1)
         {
             RCMovement.RotateAction(rotAction);
         }

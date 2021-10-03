bb57a32658da3eb5ff0adce9f1a620f4fc3e5f08
Nezzzetic <nezzzetic@gmail.com>
2021-10-03 22:12:28 +0300

    Fxes

--- Assets/Scripts/CommandProcessor.cs
+++ Assets/Scripts/CommandProcessor.cs
@@ -9,12 +9,15 @@ public class CommandProcessor : MonoBehaviour
     public SimpleMovement Movement;
     public SimpleBattery SimpleBattery;
     public Transform FXNest;
+    public GameObject FX;
+    public float FXtimeout;
 
     public void ProcessMoveCommand(int command)
     {
         if (SimpleBattery.Battery<0) return;
         moveCommand = command;
         Movement.SetMoveState(command);
+        //CreateFx();
     }
     
     public void ProcessRotCommand(int command)
@@ -22,6 +25,16 @@ public class CommandProcessor : MonoBehaviour
         if (SimpleBattery.Battery<0) return;
         rotCommand = command;
         Movement.SetRotState(command);
+        //CreateFx();
     }
+
+    void CreateFx()
+    {
+        if (FXtimeout>0) return;
+        FX.SetActive(true);
+        FXtimeout=0.5f;
+    }
+
+
     
 }
